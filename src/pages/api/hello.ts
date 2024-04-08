737db2f50a1e1199736aa04b8a58e548d08df8d9
Alex Teixeira <alex.teixeira@orium.com>
2024-04-08 14:32:09 -0300

    flag to disable external request

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -7,7 +7,16 @@ export const config = {
 
 declare const EdgeRuntime: any;
 
-export default async function handler() {
+export default async function handler(request: Request) {
+
+
+  
+  if (request.headers.get("no-external-request")) {
+    return Response.json({ name: "API Edge Runtime. no external request.", 
+    region: ""+process.env.VERCEL_REGION, 
+    isEdge: typeof EdgeRuntime !== 'string' ? false : true,
+     process: process });
+  }
 
   const r = await fetch("https://api.vercel.com/v4/aliases/example.vercel.app?from=1540095775951&projectId=prj_12HKQaOmR5t5Uy6vdcQsNIiZgHGB&since=1540095775941&teamId=SOME_STRING_VALUE&until=1540095775951", {
     "headers": {
